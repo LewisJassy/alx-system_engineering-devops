70c2346a27ae8f5d51001346dfe65810c2c92f55
LewisJassy <jassylewis70@gmail.com>
2024-04-22 23:35:12 +0300

    use an exception that handle specific error

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -25,8 +25,8 @@ def get_user_by_userid(user_id):
     try:
         user.raise_for_status()
         return user.json()
-    except:
-        exit(1)
+    except Exception as e:
+                print(f"Error processing task: {e}")
 
 
 def export_json_user_todos(user_id):
