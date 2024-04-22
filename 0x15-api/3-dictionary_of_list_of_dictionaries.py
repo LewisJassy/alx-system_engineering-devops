c99180af8ee4604ff6afc6bc7cb2df1236887800
LewisJassy <jassylewis70@gmail.com>
2024-04-22 23:27:57 +0300

    debug to use pycodestyle

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,47 +1,36 @@
 #!/usr/bin/python3
-"""Script to get information from the TODO api endpoint as it
-pertains to a particular employee identified by ID."""
-import json
-import requests
-
-
-todo_endpoint = "https://jsonplaceholder.typicode.com/todos"
-user_endpoint = "https://jsonplaceholder.typicode.com/users"
-
-
-def get_all_todos():
-    """Get TODO lists for all users identified by `user_id`"""
-    todos = requests.get(todo_endpoint)
-    try:
-        return todos.json()
-    except:
-        exit(1)
-
-
-def get_all_users():
-    """Get all users"""
-    users = requests.get(user_endpoint)
-    try:
-        return users.json()
-    except:
-        exit(1)
-
-
-def export_json_user_todos():
-    """Export employee TODO lists to a json file"""
-    todos = get_all_todos()
-    users = get_all_users()
-    data = {
-        u['id']: [dict(task=todo.get('title'),
-                       completed=todo.get('completed'),
-                       username=u.get('username'))
-                  for todo in filter(
-                          lambda t: t.get('userId') == u.get('id'), todos
-                  )] for u in users
-    }
-    with open("todo_all_employees.json", 'w') as f:
-        json.dump(data, f)
-
-
+"""script for parsing web data from an api
+"""
 if __name__ == "__main__":
-    export_json_user_todos()
+    import json
+    import requests
+    import sys
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    try:
+        employee_id = sys.argv[1]
+    except:
+        print('Usage: {} employee_id'.format(sys.argv[0]))
+        exit(1)
+
+    # grab the info about the user
+    url = base_url + 'users?id={}'.format(employee_id)
+    response = requests.get(url)
+    user = json.loads(response.text)
+    name = user[0].get('name')
+
+    # grab the info about the user's tasks
+    url = base_url + 'todos?userId={}'.format(employee_id)
+    response = requests.get(url)
+    objs = json.loads(response.text)
+    completed = 0
+    completed_tasks = []
+    for obj in objs:
+        if obj.get('completed'):
+            completed_tasks.append(obj)
+            completed += 1
+
+    # print the output about user's task completion
+    print("{} is done with tasks({}/{}):".format(name, completed, len(objs)))
+    # print the output title of completed tasks
+    for task in completed_tasks:
+        print("\t {}".format(task.get('title')))
